f5a149444d627bb8edeba37f3b787fc899a95c1e
Stephen Belyea <s.belyea@vehikl.com>
2017-11-29 22:00:25 -0500

    Sample request from open api.

--- src/views/Requests.js
+++ src/views/Requests.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Main from '../components/Main';
 
 
-const Requests = props => (
-  <Main id="requests">
-    <header>
-      <h1>Requests</h1>
-      <p>External data requests with content updates.</p>
-    </header>
-    
-  </Main>
-);
+const fetchSampleResult = () => {
+  axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(result => console.log('data', result.data))
+    .catch(error => console.log('error', error));
+};
+
+
+class Requests extends Component {
+  componentDidMount() {
+    fetchSampleResult();
+  }
+
+  render() {
+    return (
+      <Main id="requests">
+        <header>
+          <h1>Requests</h1>
+          <p>External data requests with content updates.</p>
+        </header>
+        
+      </Main>
+    );
+  }
+}
 
 
 export default Requests;
